311070dd92d74be2acf5e29ca1cf80db8533e567
Rachael Bosley <rkbosley@gmail.com>
2015-06-23 15:58:07 -0400

    no message

--- blockly/tests/navigation.js
+++ blockly/tests/navigation.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
-*Copyright [2015] [Luna Meier]
+*Copyright [2015] [Luna Meier, Rachael Bosley]
 *
 *Licensed under the Apache License, Version 2.0 (the "License");
 *you may not use this file except in compliance with the License.
