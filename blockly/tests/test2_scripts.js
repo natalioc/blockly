220227cf8d5eeacfda069ed6a3cd3d439c8ce41c
Wil Merchant <wtmercha@g.coastal.edu>
2015-07-09 10:44:43 -0400

    adding scripts to be read by the in application screen reader for task 2

---
+++ blockly/tests/test2_scripts.js
@@ -0,0 +1,23 @@
+//Audio Description Scripts for part 2 
+
+//Training Example from stefik 
+var array = "variable i equals 0. variable k equals 0. while i less than 2, increase k by 1, if i is equal to 0 increase k by 1, else decrease k by 1, increase i by 1, print k";
+
+
+
+//Example 6 from Stefik 
+var task2_text1 = "variable i equals 0. variable d equals 4. If d not equals d. While i less than d, if 2 equals i or 1 equals i, if 2 equals i, else, if 0 equals i. if 0 equals i. increase i by 1. if d equals d.";
+
+//CSCI140 examples
+
+//1)
+var task2_1 = "variable count equals 1. variable y equals 6. while count less than 6, increase y by i, increase count by 1. print y.";
+
+//KW code
+var task2_kw = [];
+task2_kw[0] = "variable i equals 0";
+task2_kw[1] = "loop 10 times";
+task2_kw[2] = "print i";
+task2_kw[3] = "if i equals 5";
+task2_kw[4] = "print Halfway";
+task2_kw[5] = "increase i by 1";
